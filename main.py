df51b8e3bf4409e9384eb7e91ee365ff13853a49
Dimas Bangkit Satriawan <57074351+smdbngkt@users.noreply.github.com>
2023-02-11 21:21:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,21 @@ time.sleep(1)
 print("1")
 time.sleep(1)
 
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+      
+    print('Fire in the hole!!')
+  
+  
+# input time in seconds
+t = waktu1
+
 
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -46,20 +61,7 @@ while True:
         r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
         print(Fore.WHITE + "Sent message: ")
         print(Fore.YELLOW + payload['content'])
-        def countdown(t):
-    
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
-      
-    print('Fire in the hole!!')
-  
-  
-# input time in seconds
-t = waktu1
+        
   
 # function call
 
